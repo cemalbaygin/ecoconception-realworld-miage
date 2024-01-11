7fb2a2f7fe08da05236938fc4e4568517b636465
cemal <cemalbgn@gmail.cpm>
2024-01-11 14:08:49 +0100

    Respecter le principe de navigation rapide dans l’historique

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,26 @@
-import { Component } from "@angular/core";
+import { Component, OnDestroy, OnInit } from "@angular/core";
+import { fromEvent, Subject } from 'rxjs';
+import { takeUntil } from 'rxjs/operators';
 
 @Component({
   selector: "app-root",
   templateUrl: "./app.component.html",
 })
-export class AppComponent {}
+export class AppComponent implements OnInit, OnDestroy{
+  private unsubscriber : Subject<void> = new Subject<void>();
+
+
+  ngOnInit(): void {
+    history.pushState(null, '');
+
+    fromEvent(window, 'popstate').pipe(
+      takeUntil(this.unsubscriber)
+    ).subscribe((_) => {
+      history.pushState(null, '');
+    });
+  }
+  ngOnDestroy(): void {
+    this.unsubscriber.next();
+    this.unsubscriber.complete();
+  }
+}
